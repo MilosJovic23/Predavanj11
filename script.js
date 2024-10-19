2aa912f98b5f43de1662b51fd27f982217647b23
MilosJovic23 <milos.mj992@gmail.com>
2024-10-19 21:39:23 +0200

    * small changes

--- script.js
+++ script.js
@@ -8,11 +8,11 @@
 // 	});
 // }
 
-// document.getElementById("paraHolder").addEventListener("click", function (e) {
-// 	var target = e.target; // event target = element na koji se kliknulo
-// 	target.style.color = "blue";
-// 	console.log(target);
-// });
+document.getElementById("paraHolder").addEventListener("click", function (e) {
+	var target = e.target; // event target = element na koji se kliknulo
+	target.style.color = "blue";
+	console.log(target);
+});
 
 document.getElementById("imageHolder").addEventListener("click", function (e) {
 	e.target.classList.toggle("zoom");
